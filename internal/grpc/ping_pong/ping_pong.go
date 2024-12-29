eb4757074b3f8f490598408c3a1c9357843df044
spp28 <sppetrovich@mail.ru>
2024-12-29 10:04:31 +0200

    rpc: added a logging to ping ping

--- internal/grpc/ping_pong/ping_pong.go
+++ internal/grpc/ping_pong/ping_pong.go
@@ -2,6 +2,7 @@ package ping_pong
 
 import (
 	"context"
+	"log/slog"
 	gen "slogger-transporter/internal/grpc/gen/services/ping_pong_gen"
 )
 
@@ -10,5 +11,7 @@ type PingPongServer struct {
 }
 
 func (p *PingPongServer) Ping(ctx context.Context, in *gen.Request) (*gen.Response, error) {
+	go slog.Info("Ping: " + in.Message)
+
 	return &gen.Response{Message: in.Message}, nil
 }
